383c19af38b1b958e50cfbacc8205a91fc6c6a76
satchmo496 <46792650+satchmo496@users.noreply.github.com>
2024-11-22 13:14:16 -0500

    Update mgr.js

--- mgr.js
+++ mgr.js
@@ -9,7 +9,7 @@ define(['managerAPI',
 
 	var API    = new Manager();
 	//const subid = Date.now().toString(16)+Math.floor(Math.random()*10000).toString(16);
-	init_data_pipe(API, 'zpjKaPesdEOI',  {file_type:'csv'});	
+	init_data_pipe(API, 'ZAvobo44YJdA',  {file_type:'csv'});	
 
     API.setName('mgr');
     API.addSettings('skip',true);
